47d609949077e7810ee4c1260b00c340827fac8a
armaanrana6 <43334013+armaanrana6@users.noreply.github.com>
2018-11-13 20:59:23 +0530

    Add files via upload

---
+++ AssignmentC#(ITD18006)/A2ArmaanRanaP1/A2ArmaanRanaP1/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2ArmaanRanaP1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double tuition, registration, tax , total ;
+            Console.WriteLine("International Student (Yes or No) ");
+            string student = Console.ReadLine();
+            Console.WriteLine("Enter age ");
+            string ag = Console.ReadLine();
+            int age = int.Parse(ag);
+              Console.WriteLine("Registration month");
+           string month = Console.ReadLine();
+           
+
+            Console.WriteLine("Student's Age  " + age);
+            Console.WriteLine("International Student (or not)" + student);
+
+            
+           
+            if (student == "No")
+            {
+                if (age <= 18)
+                {
+                    tuition = 300 + (0.13* 300);
+                    Console.WriteLine("Base Tution $" + tuition);
+                }
+
+                else if  (age <= 49)
+                {
+                    tuition = 500 + (0.13* 500);
+                    Console.WriteLine("Base Tution $" + tuition);
+                }
+                else if (age >= 50)
+                {
+                    tuition = 400 + (0.13 * 400);
+                    Console.WriteLine("Base Tution $" + tuition);
+
+                }
+            }
+           else if (student == "Yes")
+            {
+
+                if (age <= 18)
+                {
+                    tuition = 100 + 300 + (0.13 * 300);
+                    Console.WriteLine("International Student Fee $" + tuition);
+                }
+
+                if (age>=19 && age <= 49)
+                {
+                    tuition =100 + 500 + (0.13 * 500);
+                    Console.WriteLine("International Student Fee $" + tuition);
+                }
+                if (age >= 50)
+                {
+                    tuition = 100 + 400 + (0.13 * 400);
+                    Console.WriteLine("International Student Fee $" + tuition);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter Yes or No");
+            }
+
+
+            switch (month)
+            {
+                case "September":
+                    {
+
+                        Console.WriteLine("Registration Semester : Fall");
+                        registration = 250 + (0.13 * 250);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 250 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "October":
+                    {
+                        Console.WriteLine("Registration Semester : Fall");
+                        registration = 250 + (0.13 * 250);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 250 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "November":
+                    {
+                        Console.WriteLine("Registration Semester : Fall");
+                        registration = 250 + (0.13 * 250);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 250 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "December":
+                    {
+                        Console.WriteLine("Registration Semester : Fall");
+                        registration = 250 + (0.13 * 250);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 250 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "January":
+                    {
+                        Console.WriteLine("Registration Semester : Winter");
+                        registration = 220 + (0.13 * 220);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 220 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "February":
+                    {
+                        Console.WriteLine("Registration Semester : Winter");
+                        registration = 220 + (0.13 * 220);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 220 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "March":
+                    {
+
+                        Console.WriteLine("Registration Semester : Winter");
+                        registration = 220 + (0.13 * 220);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 220 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "April":
+                    {
+                        Console.WriteLine("Registration Semester : Winter");
+                        registration = 220 + (0.13 * 220);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 220 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+
+                case "May":
+                    {
+                        Console.WriteLine("Registration Semester : Summer");
+                        registration = 150 + (0.13 * 150);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 150 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "June":
+                    {
+                        Console.WriteLine("Registration Semester : Summer");
+                        registration = 150 + (0.13 * 150);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 150 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "July":
+                    {
+                        Console.WriteLine("Registration Semester : Summer");
+                        registration = 150 + (0.13 * 150);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 150 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+                case "August":
+                    {
+                        Console.WriteLine("Registration Semester : Summer");
+                        registration = 150 + (0.13 * 150);
+                        Console.WriteLine("Registration Fee for semester $" + registration);
+                        tax = 150 * 13 / 100;
+                        Console.WriteLine("HST $" + tax);
+                        break;
+                    }
+
+                default:
+                    {
+                        Console.WriteLine("Enter valid Semester");
+                        break;
+                    }
+
+
+            }
+           /* total = tax + tuition + registration ;
+            Console.WriteLine("Total Fee " +total );
+            */
+
+
+        }
+    }
+}
---
+++ AssignmentC#(ITD18006)/A2ArmaanRanaP2/A2ArmaanRanaP2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2ArmaanRanaP2
+{
+    class Program
+    {
+        static double sphere(double r)
+        {
+            double sphere;
+            sphere = 4 / 3 * 3.14 * r * r * r;
+            return sphere;
+        }
+        static double sphere(double r, double h)
+        {
+            double sphere;
+            sphere = 3.14 * r * r * h;
+            return sphere;
+        }
+        static double sphere(int l, int b, int h)
+        {
+            double sphere;
+            sphere = l * b * h;
+            return sphere;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select the type of Shape");
+            Console.WriteLine("1:Sphere");
+            Console.WriteLine("2:Cylinder");
+            Console.WriteLine("3:Rectangular Prism");
+            string x = Console.ReadLine();
+            int y = int.Parse(x);
+            if (y == 1)
+            {
+                Console.WriteLine("enter the value of radius");
+                string u = Console.ReadLine();
+                double r = double.Parse(u);
+                double result;
+                result = sphere(r);
+                Console.WriteLine("Volume of sphere = " + result);
+            }
+            else if (y == 2)
+            {
+                Console.WriteLine("enter the value of radius");
+                string i = Console.ReadLine();
+                double r = double.Parse(i);
+                Console.WriteLine("enter the value of height");
+                string k = Console.ReadLine();
+                double h = double.Parse(i);
+                double res;
+                res = sphere(r, h);
+                Console.WriteLine("Volume of cylinder =" + res);
+            }
+            else if (y == 3)
+            {
+                Console.WriteLine("enter the length of rectangle");
+                string a = Console.ReadLine();
+                int l = int.Parse(a);
+                Console.WriteLine("enter the breadth of rectangle");
+                string c = Console.ReadLine();
+                int b = int.Parse(c);
+                Console.WriteLine("enter the height of rectangle");
+                string d = Console.ReadLine();
+                int h = int.Parse(d);
+                double resu;
+                resu = sphere(l, b, h);
+                Console.WriteLine("Volume of rectangular prism is " + resu);
+            }
+        }
+    }
+}
+    
